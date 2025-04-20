b1fd17fb60a6ea7b40948ac05c8c81e0cd67d95e
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-04-20 20:15:15 +0530

    second approach to check if a number is Palindrome or not

--- script.js
+++ script.js
@@ -10,15 +10,22 @@
 // };
 // console.log(twoSum([2, 7, 11, 15], 9));
 
+// // 9. Palindrome Number
+// const isPalindrome = function (x) {
+//   let copyNumber = x;
+//   let sum = 0;
+//   while (x > 0) {
+//     let rem = x % 10;
+//     sum = sum * 10 + rem;
+//     x = Math.floor(x / 10);
+//   }
+//   return sum === copyNumber ? true : false;
+// };
+// console.log(isPalindrome(121));
+
 // 9. Palindrome Number
 const isPalindrome = function (x) {
-  let copyNumber = x;
-  let sum = 0;
-  while (x > 0) {
-    let rem = x % 10;
-    sum = sum * 10 + rem;
-    x = Math.floor(x / 10);
-  }
-  return sum === copyNumber ? true : false;
+  const reverseNumber = Number(x.toString().split("").reverse().join(""));
+  return x === reverseNumber ? true : false;
 };
-console.log(isPalindrome(121));
+console.log(isPalindrome(123));
