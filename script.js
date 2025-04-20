0ee40cb5e54de8a9ce9b65f1b6869100f70e0df0
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-04-20 20:11:06 +0530

    9. Palindrome Number

---
+++ script.js
@@ -0,0 +1,26 @@
+// // 1. Two Sum
+// const twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     for (let j = 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+// console.log(twoSum([2, 7, 11, 15], 9));
+
+// 9. Palindrome Number
+const isPalindrome = function (x) {
+  let copyNumber = x;
+  let sum = 0;
+  while (x > 0) {
+    let rem = x % 10;
+    sum = sum * 10 + rem;
+    x = Math.floor(x / 10);
+  }
+  console.log(copyNumber, x);
+  
+  return sum === copyNumber ? true : false;
+};
+console.log(isPalindrome(121));
