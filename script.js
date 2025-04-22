66aefda64afbdabf32a80be93d1714164826f746
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-04-22 20:10:18 +0530

    remove element from an array

--- script.js
+++ script.js
@@ -30,17 +30,29 @@
 // };
 // console.log(isPalindrome(121));
 
-// 26. Remove Duplicates from Sorted Array
-const removeDuplicates = function (nums) {
-    if(nums.length === 0) return 0;
+// // 26. Remove Duplicates from Sorted Array
+// const removeDuplicates = function (nums) {
+//     if(nums.length === 0) return 0;
+//     let unique = 0;
+//     for(let i = 1; i<nums.length; i++){
+//         if(nums[unique] !== nums[i]){
+//             unique++;
+//             nums[unique] = nums[i];
+//         }
+//     }
+//     return unique + 1;
+// };
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
 
-    let unique = 0;
-    for(let i = 1; i<nums.length; i++){
-        if(nums[unique] !== nums[i]){
-            unique++;
-            nums[unique] = nums[i];
-        }
+// 27. Remove Element
+const removeElement = function (nums, val) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
     }
-    return unique + 1;
+  }
+  return k;
 };
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+console.log(removeElement([3, 2, 2, 3], 3));
