97518273bcb74cd1deda94891ad0260862bf511f
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-04-29 16:54:41 +0530

    leetcode 202. Happy Number using set O(1) Time Complexity

--- script.js
+++ script.js
@@ -260,9 +260,32 @@
 // };
 // console.log(isPalindrome("A man, a plan, a canal: Panama"));
 
+// // 202. Happy Number
+// const isHappy = function (n) {
+//   let arr = new Array();
+//   while (n !== 1) {
+//     let sum = 0;
+//     while (n > 0) {
+//       let rem = n % 10;
+//       sum = sum + rem * rem;
+//       n = Math.floor(n / 10);
+//     }
+
+//     if (sum === 1) return true;
+//     if (arr.includes(sum)) return false;
+//     else arr.push(sum);
+
+//     n = sum;
+//   }
+
+//   return true;
+// };
+// console.log(isHappy(19));
+
 // 202. Happy Number
 const isHappy = function (n) {
-  let arr = new Array();
+  const set = new Set();
+
   while (n !== 1) {
     let sum = 0;
     while (n > 0) {
@@ -272,8 +295,8 @@ const isHappy = function (n) {
     }
 
     if (sum === 1) return true;
-    if (arr.includes(sum)) return false;
-    else arr.push(sum);
+    if (set.has(sum)) return false;
+    else set.add(sum);
 
     n = sum;
   }
