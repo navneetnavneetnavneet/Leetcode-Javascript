d8cb4f8e7c6c1cf82b287bb702a5ad34178cf897
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-01 17:11:28 +0530

    1. Two Sum

--- script.js
+++ script.js
@@ -658,3 +658,27 @@
 // console.log(generate(5));
 
 
+// // 1. Two Sum
+// function twoSum(nums, target){
+//   for(let i=0; i<nums.length-1; i++){
+//     for(let j=i+1; j<nums.length; j++){
+//       if(nums[i] + nums[j] === target){
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
+// console.log(twoSum([2,7,11,15], 9));
+
+// // 1. Two Sum
+// function twoSum(nums, target){
+//   const map = new Map();
+//   for(let i=0; i<nums.length; i++){
+//     let complement = target - nums[i];
+//     if(map.has(complement)){
+//       return [map.get(complement), i];
+//     }
+//     map.set(nums[i], i);
+//   }
+// }
+// console.log(twoSum([2,7,11,15], 9));
