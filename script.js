07b2e0bc8f68c1e14c6c6d56ca74053fb1eab35b
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-02 19:49:48 +0530

    Array Right Rotation by 1 Step

--- script.js
+++ script.js
@@ -846,7 +846,17 @@
 //     arr[i] = arr[i + 1];
 //   }
 //   arr[arr.length - 1] = temp;
-
 //   return arr;
 // }
 // console.log(leftRotaion([1, 2, 3, 4, 5]));
+
+// // Array Right Rotation by 1 Step
+// function rightRotaion(arr) {
+//   let temp = arr[arr.length - 1];
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     arr[i] = arr[i - 1];
+//   }
+//   arr[0] = temp;
+//   return arr;
+// }
+// console.log(rightRotaion([1, 2, 3, 4, 5]));
