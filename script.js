df9d08c7145fed43d6539b28f077ea3a9fde6d1a
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-02 09:51:22 +0530

    283. Move Zeroes

--- script.js
+++ script.js
@@ -776,3 +776,40 @@
 //   return ans;
 // }
 // console.log(missingNumber([9,6,4,2,3,5,7,0,1]));
+
+// // 283. Move Zeroes
+// function moveZeroes(nums) {
+//   let nonZero = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== 0) {
+//       nums[nonZero] = nums[i];
+//       nonZero++;
+//     }
+//   }
+//   for (let i = nonZero; i < nums.length; i++) {
+//     nums[i] = 0;
+//   }
+//   return nums;
+// }
+// console.log(moveZeroes([0, 1, 0, 3, 12]));
+
+// // 283. Move Zeroes
+// function moveZeroes(nums) {
+//   let j;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === 0) {
+//       j = i;
+//       break;
+//     }
+//   }
+//   for (let i = j + 1; i < nums.length; i++) {
+//     if (nums[i] !== 0) {
+//       let temp = nums[i];
+//       nums[i] = nums[j];
+//       nums[j] = temp;
+//       j++;
+//     }
+//   }
+//   return nums;
+// }
+// console.log(moveZeroes([0,1,0,3,12]));
