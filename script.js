f76b5b153ab2113660096995301823ce98814c1c
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-03 18:51:36 +0530

    121. Best Time to Buy and Sell Stock

--- script.js
+++ script.js
@@ -1008,3 +1008,18 @@
 //   return maxProfit;
 // }
 // console.log(maxProfit([7, 6, 4, 3, 1]));
+
+// // 121. Best Time to Buy and Sell Stock
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+  }
+  return maxProfit;
+}
+console.log(maxProfit([7,6,4,3,1]));
