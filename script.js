56b8d6e528e8fd2bc3db1dfedc85a2702d6b6e0a
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-04 19:38:32 +0530

    35. Search Insert Position

--- script.js
+++ script.js
@@ -1054,3 +1054,21 @@
 //   return nums;
 // }
 // console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
+
+// // 35. Search Insert Position
+// function search(nums, target) {
+//   let start = 0;
+//   let end = nums.length - 1;
+//   while (start <= end) {
+//     let mid = Math.floor(start + (end - start) / 2);
+//     if (nums[mid] === target) {
+//       return start = mid;
+//     } else if (nums[mid] > target) {
+//       end = mid - 1;
+//     } else if (nums[mid] < target) {
+//       start = mid + 1;
+//     }
+//   }
+//   return start
+// }
+// console.log(search([1, 3, 5, 6], 5));
