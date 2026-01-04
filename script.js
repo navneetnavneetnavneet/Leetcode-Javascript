8559d55252e44077ecb1ae5162e7d353f2b9e6a4
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-04 18:58:32 +0530

    189. Rotate Array

--- script.js
+++ script.js
@@ -1035,3 +1035,22 @@
 //   return maxProfit;
 // }
 // console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+
+// // 189. Rotate Array
+// function reverse(arr, i, j) {
+//   while (i < j) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++;
+//     j--;
+//   }
+// }
+// function rotate(nums, k) {
+//   k = k % nums.length;
+//   reverse(nums, 0, nums.length - 1);
+//   reverse(nums, 0, k-1);
+//   reverse(nums, k, nums.length - 1);
+//   return nums;
+// }
+// console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
