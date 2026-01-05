4376cb7791076c7229754a872ba5288719ada95a
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-05 10:03:10 +0530

    1572. Matrix Diagonal Sum

--- script.js
+++ script.js
@@ -1096,3 +1096,23 @@
 //     [18, 19, 20],
 //   ])
 // );
+
+// // 1572. Matrix Diagonal Sum
+// function diagonalSum(mat) {
+//   let sum = 0;
+//   for (let i = 0; i < mat.length; i++) {
+//     for (let j = 0; j < mat[i].length; j++) {
+//       if (i === j || i + j === mat.length - 1) {
+//         sum = sum + mat[i][j];
+//       }
+//     }
+//   }
+//   return sum;
+// }
+// console.log(
+//   diagonalSum([
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ])
+// );
