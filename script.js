d1dbd33f1498045d6042236cb1687b610867aa74
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-05 20:00:29 +0530

    54. Spiral Matrix

--- script.js
+++ script.js
@@ -1166,3 +1166,46 @@
 //     [7, 8, 9],
 //   ])
 // );
+
+// // 54. Spiral Matrix
+// function spiralOrder(matrix) {
+//   let result = [];
+//   let top = 0;
+//   let left = 0;
+//   let right = matrix[0].length - 1;
+//   let bottom = matrix.length - 1;
+
+//   while (top <= bottom && left <= right) {
+//     for (let i = top; i <= right; i++) {
+//       result.push(matrix[top][i]);
+//     }
+//     top++;
+
+//     for (let i = top; i <= bottom; i++) {
+//       result.push(matrix[i][right]);
+//     }
+//     right--;
+
+//     if (top <= bottom) {
+//       for (let i = right; i >= left; i--) {
+//         result.push(matrix[bottom][i]);
+//       }
+//       bottom--;
+//     }
+
+//     if (left <= right) {
+//       for (let i = bottom; i >= top; i--) {
+//         result.push(matrix[i][left]);
+//       }
+//       left++;
+//     }
+//   }
+//   return result;
+// }
+// console.log(
+//   spiralOrder([
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ])
+// );
