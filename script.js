a91c87f2ab5db21b7cdae61461fb983921f78389
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-06 19:03:18 +0530

    28. Find the Index of the First Occurrence in a String

--- script.js
+++ script.js
@@ -1252,3 +1252,9 @@
 //   return result;
 // }
 // console.log(generateSpiralMatrix(3));
+
+// // 28. Find the Index of the First Occurrence in a String
+// function strStr(haystack, needle) {
+//   return haystack.indexOf(needle);
+// }
+// console.log(strStr("sadbutsad", "sad"));
