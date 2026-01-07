46a2ce1f9dfae8f561a3cb3a8e51674363251f17
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2026-01-07 13:46:00 +0530

    Insertion Sort Algorithm

--- script.js
+++ script.js
@@ -993,6 +993,21 @@
 // }
 // console.log(selectionSort([5, 3, 1, 7, 9]));
 
+// // Insertion Sort Algorithm
+// function insertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     let key = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > key) {
+//       arr[j + 1] = arr[j];
+//       j--;
+//     }
+//     arr[j + 1] = key;
+//   }
+//   return arr;
+// }
+// console.log(insertionSort([5, 3, 1, 9, 7, 2]));
+
 // // 53. Maximum Subarray (Time Limit Exceeded)
 // function maxSubArray(nums) {
 //   let maxSum = nums[0];
